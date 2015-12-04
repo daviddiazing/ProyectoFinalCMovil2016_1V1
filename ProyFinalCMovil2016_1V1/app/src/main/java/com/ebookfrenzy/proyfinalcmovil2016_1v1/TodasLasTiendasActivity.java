e8a8c49417f690cdb15c9e0e38d14277957afcdf
daviddiazing <daviddiazing@gmail.com>
2015-12-03 18:04:26 -0600

    decimo segunda confirmación, contiene la integración de ubicar tienda completa

--- ProyFinalCMovil2016_1V1/app/src/main/java/com/ebookfrenzy/proyfinalcmovil2016_1v1/TodasLasTiendasActivity.java
+++ ProyFinalCMovil2016_1V1/app/src/main/java/com/ebookfrenzy/proyfinalcmovil2016_1v1/TodasLasTiendasActivity.java
@@ -2,7 +2,7 @@ package com.ebookfrenzy.proyfinalcmovil2016_1v1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//gps
 import android.os.IBinder;
 import android.content.Context;
 import android.content.Intent;
@@ -19,10 +19,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
-
+//xml
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.net.ConnectivityManager;
@@ -41,6 +42,9 @@ public class TodasLasTiendasActivity extends AppCompatActivity {
 
     BoundService myService;//gps
     boolean isBound = false;//gps
+		
+	ArrayList<Lista_entrada> datos = new ArrayList<Lista_entrada>();
+	ArrayList<Lista_entrada> datos2 = new ArrayList<Lista_entrada>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,8 +53,10 @@ public class TodasLasTiendasActivity extends AppCompatActivity {
 
         Intent intent = new Intent(this, BoundService.class);//levantar servicio gps
         bindService(intent, myConnection, Context.BIND_AUTO_CREATE);//levantar servicio gps
-
-		rellenarTiendas();
+		
+			
+			rellenarTiendas();
+			
 		
         //double currentTime[] = myService.getCurrentTime();//levantar servicio gps
     }
@@ -165,7 +171,7 @@ public class TodasLasTiendasActivity extends AppCompatActivity {
         protected void onPostExecute(Boolean success) {
             if(success){
                 try {
-                    inicializaListView();
+                    // inicializaListView();
                     // pDialog.dismiss();
 
                     // placeHolder.tvNombre.setText(juegos.get(position).getNombre());
@@ -175,28 +181,195 @@ public class TodasLasTiendasActivity extends AppCompatActivity {
 
 					double currentTime[] = myService.getCurrentTime();//levantar servicio gps
 					
-                    String nombre="";
+                    // String nombre="";
                     for (int i =0;i<ArrayTiendas.size();i++)
                     {
-                        nombre = nombre + "\n" + ArrayTiendas.get(i).getNombre() + "," + ArrayTiendas.get(i).getLatitud() + ","+ ArrayTiendas.get(i).getLongitud();
+                        // nombre = nombre + "\n" + ArrayTiendas.get(i).getNombre() + "," + ArrayTiendas.get(i).getLatitud() + ","+ ArrayTiendas.get(i).getLongitud();
+						
+						String nombre = ArrayTiendas.get(i).getNombre();
+						String latitud = ArrayTiendas.get(i).getLatitud();
+						String longitud = ArrayTiendas.get(i).getLongitud();
+						
+						if(ArrayTiendas.get(i).getNombre().equalsIgnoreCase("Walmart Copilco"))
+							datos.add(new Lista_entrada(R.drawable.im_walmart, "WALMART", "COPILCO",nombre,latitud,longitud));
+						if(ArrayTiendas.get(i).getNombre().equalsIgnoreCase("Mega Comercial Mexicana Quevedo"))
+							datos.add(new Lista_entrada(R.drawable.im_mega, "MEGA", "QUEVEDO",nombre,latitud,longitud));
+						if(ArrayTiendas.get(i).getNombre().equalsIgnoreCase("Chedraui Copilco"))
+							datos.add(new Lista_entrada(R.drawable.im_chedraui, "CHEDRAUI", "COPILCO",nombre,latitud,longitud));
                     }
 
 					// i.putExtra("Latitud", currentTime[0]);
 					// i.putExtra("Longitud", currentTime[1]);
-					nombre = nombre + "\n\n\n" + "coordenadas GPS: " + currentTime[0] + "," + currentTime[1];
+					// nombre = nombre + "\n\n\n" + "coordenadas GPS: " + currentTime[0] + "," + currentTime[1];
 
 
-                    TextView myTextView =
-                            (TextView)findViewById(R.id.textView);
-                    myTextView.setText(nombre);
+                    // // // // // TextView myTextView =
+                            // // // // // (TextView)findViewById(R.id.textView);
+                    // // // // // myTextView.setText(nombre);
                     // myTextView.setText(stackSites.get(0).toString()); //se trae toda una entrada
 
+			
+			// ArrayList<Lista_entrada> datos = new ArrayList<Lista_entrada>();
+			// datos.add(new Lista_entrada(R.drawable.im_walmart, "WALMART", "COPILCO"));
+			// datos.add(new Lista_entrada(R.drawable.im_mega, "MEGA", "QUEVEDO"));
+			// datos.add(new Lista_entrada(R.drawable.im_chedraui, "CHEDRAUI", "COPILCO"));
+					datos2 = obtenerTiendasOrdenadas(datos, currentTime);
+			
+				
+					Intent intent = new Intent(TodasLasTiendasActivity.this,ListadoTiendasActivity.class);
+					// intent.putExtra("DATO_2",elegido);
+					intent.putExtra("DATO",datos2);
+					startActivity(intent);			
+					finish();
+					
                 }catch (Exception e){
                     e.printStackTrace();
                 }
             }else{
                 Toast.makeText(context,"Error de Lectura",Toast.LENGTH_SHORT).show();
             }
-        }
+        }//fin de onPostExecute
+		
+		public ArrayList<Lista_entrada> obtenerTiendasOrdenadas(ArrayList<Lista_entrada> ArrayTiendas, double currentTime[])
+		{
+			ArrayList<Lista_entrada> ArrayTiendasOrdenadas = new ArrayList<Lista_entrada>();
+			double distancia[] = new double[ArrayTiendas.size()];
+			double distancia2[] = new double[ArrayTiendas.size()];
+			
+			String d_str[] = new String[ArrayTiendas.size()];
+			String d2_str[] = new String[ArrayTiendas.size()];
+
+			double xgps = currentTime[0];//Latitud
+			double ygps = currentTime[1];//Longitud
+			// double xgps = 19.333943;//Latitud
+			// double ygps = -99.154865;//Longitud
+			double xtienda=0.0;//Latitud
+			double ytienda=0.0;//Longitud
+			
+			double dx=0.0;//distancia en x (latitud) entre la tienda y la posicion gps
+			double dy=0.0;//distancia en y (longitud) entre la tienda y la posicion gps
+			
+			for(int i=0;i<ArrayTiendas.size();i++)
+			{
+				xtienda = Double.parseDouble(ArrayTiendas.get(i).get_latitud());
+				ytienda = Double.parseDouble(ArrayTiendas.get(i).get_longitud());
+				if(xtienda-xgps<0)//obtener distancia positiva en x (latitud)
+				{
+					dx = xgps-xtienda;
+				}else{
+					dx = xtienda-xgps;
+				}
+				
+				if(ytienda-ygps<0)//obtener distancia positiva en y (longitud)
+				{
+					dy = ygps-ytienda;
+				}else{
+					dy = ytienda-ygps;
+				}
+				
+				distancia[i]=Math.sqrt(Math.pow(dx,2)  +   Math.pow(dy,2));				
+				distancia2[i] = distancia[i];
+				d_str[i] = String.valueOf(distancia[i]);
+				d2_str[i] = String.valueOf(distancia[i]);
+                // Toast.makeText(context,String.valueOf(distancia[i]),Toast.LENGTH_SHORT).show();				
+			}//fin de for
+			
+			Arrays.sort(distancia2);// ordena distancia2 en forma ascendente de menor a mayor distancia
+			// Toast.makeText(context,String.valueOf(distancia2[0]),Toast.LENGTH_SHORT).show();				
+			// Toast.makeText(context,String.valueOf(distancia2[1]),Toast.LENGTH_SHORT).show();				
+			// Toast.makeText(context,String.valueOf(distancia2[2]),Toast.LENGTH_SHORT).show();				
+			
+			Arrays.sort(d2_str);
+			
+			if(d_str[0].equals(d2_str[0]))
+            {
+				ArrayTiendasOrdenadas.add(ArrayTiendas.get(0));
+                // Toast.makeText(context,"opcion1a",Toast.LENGTH_SHORT).show();				
+            }else if(d_str[1].equals(d2_str[0]))
+			{
+				ArrayTiendasOrdenadas.add(ArrayTiendas.get(1));
+				// Toast.makeText(context,"opcion1b",Toast.LENGTH_SHORT).show();				
+			}else if(d_str[2].equals(d2_str[0]))
+			{
+				ArrayTiendasOrdenadas.add(ArrayTiendas.get(2));
+				// Toast.makeText(context,"opcion1c",Toast.LENGTH_SHORT).show();				
+			}
+			//
+			if(d_str[0].equals(d2_str[1]))
+            {
+				ArrayTiendasOrdenadas.add(ArrayTiendas.get(0));
+                // Toast.makeText(context,"opcion2a",Toast.LENGTH_SHORT).show();				
+            }else if(d_str[1].equals(d2_str[1]))
+			{
+				ArrayTiendasOrdenadas.add(ArrayTiendas.get(1));
+				// Toast.makeText(context,"opcion2b",Toast.LENGTH_SHORT).show();				
+			}else if(d_str[2].equals(d2_str[1]))
+			{
+				ArrayTiendasOrdenadas.add(ArrayTiendas.get(2));
+				// Toast.makeText(context,"opcion2c",Toast.LENGTH_SHORT).show();				
+			}
+			//
+			if(d_str[0].equals(d2_str[2]))
+            {
+				ArrayTiendasOrdenadas.add(ArrayTiendas.get(0));
+                // Toast.makeText(context,"opcion3a",Toast.LENGTH_SHORT).show();				
+            }else if(d_str[1].equals(d2_str[2]))
+			{
+				ArrayTiendasOrdenadas.add(ArrayTiendas.get(1));
+				// Toast.makeText(context,"opcion3b",Toast.LENGTH_SHORT).show();				
+			}else if(d_str[2].equals(d2_str[2]))
+			{
+				ArrayTiendasOrdenadas.add(ArrayTiendas.get(2));
+				// Toast.makeText(context,"opcion3c",Toast.LENGTH_SHORT).show();				
+			}
+			
+			// if(d_str[0].equals(d2_str[1]))
+            // {
+                // Toast.makeText(context,"son diferentes",Toast.LENGTH_SHORT).show();				
+            // }
+						
+			// int ubicacion=0;
+			// ubicacion = Arrays.binarySearch( distancia, distancia2[0] );
+			// ArrayTiendasOrdenadas.add(ArrayTiendas.get(ubicacion));
+			
+						
+			// ubicacion = Arrays.binarySearch( distancia, distancia2[1] );
+			// ArrayTiendasOrdenadas.add(ArrayTiendas.get(ubicacion));
+	
+			
+			// Toast.makeText(context,"llego aqui segundo",Toast.LENGTH_SHORT).show();				
+			
+			
+			
+			// // ubicacion = Arrays.binarySearch( distancia, distancia2[2] );
+			// // ArrayTiendasOrdenadas.add(ArrayTiendas.get(ubicacion));
+			
+			// ArrayTiendasOrdenadas.add(ArrayTiendas.get(posicionBuscada));
+			
+			// for(int i=0;i<ArrayTiendas.size();i++)
+			// {
+				// double distBuscada = distancia2[i];
+				// int posicionBuscada = 0;
+				// for(int j=0;j<ArrayTiendas.size();j++)
+				// {
+					// // if(distancia[j]==distBuscada)//busca la posicion donde esta la distBuscada
+					// if(new Double(distancia[j]).compareTo(new Double(distBuscada))==0)//busca la posicion donde esta la distBuscada
+					// {
+						
+						// posicionBuscada=j;
+						// break;
+					// }else{
+						// j++;
+					// }
+				// }//fin de for de busqueda de posicionBuscada
+				
+				// ArrayTiendasOrdenadas.add(ArrayTiendas.get(posicionBuscada));
+			// }//fin de for de cargar arreglo ordenado
+			
+			// if(ArrayTiendas.get(0).get_nombre().equalsIgnoreCase("Walmart Copilco"))
+				// Toast.makeText(context,"Error de Lectura",Toast.LENGTH_SHORT).show();
+			
+			return ArrayTiendasOrdenadas;
+		}//fin de obtenerTiendasOrdenadas
     }
 }
